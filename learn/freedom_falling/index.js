fe1306c0553867138db71ffaa38d42b87df6b706
frank <13732629211@163.com>
2019-03-04 21:41:17 +0800

    =小球自由落体修改

--- learn/freedom_falling/index.js
+++ learn/freedom_falling/index.js
@@ -6,15 +6,14 @@ window.onload = function(){
     canvas.width = WINDOW_WIDTH;
     canvas.height = WINDOW_HEIGHT;
     var context = canvas.getContext("2d");
-    
-    drawBall(context)
     setInterval(() => {
-        moveBall(context)
+        render(context)
+        update();
     }, 50);
 }
-var ball = { x:50,y:50,radius:10,vx:5,vy:3,g:1.5 };
+var ball = { x:50,y:50,radius:10,vx:5,vy:-2,g:1 };
 
-function drawBall(cxt){
+function render(cxt){
     cxt.clearRect(0,0,WINDOW_WIDTH, WINDOW_HEIGHT);
     cxt.beginPath();
     cxt.arc(  ball.x,ball.y, ball.radius, 0 , 2*Math.PI )
@@ -23,7 +22,7 @@ function drawBall(cxt){
     cxt.fill()
 }
 
-function moveBall(cxt){
+function update(){
     ball.x += ball.vx;
     ball.y += ball.vy;
     ball.vy += ball.g;
@@ -32,5 +31,4 @@ function moveBall(cxt){
         ball.y = WINDOW_HEIGHT-ball.radius;
         ball.vy = - ball.vy*0.75;
     }
-    drawBall(cxt)
 }
